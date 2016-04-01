23dc3927b34c1cb3fbd92edb68af030cef5af950
Anton Grishun <anton.grishun@kpn.com>
2016-04-01 15:39:11 +0200

    build / development optimisation, sass & caching support

--- config/webpack.dev.js
+++
@@ -1,4 +0,0 @@
-var baseConfig = require('./webpack.base.js');
-var config = Object.create(baseConfig);
-config.devtool = 'cheap-module-source-map';
-module.exports = config;
---
+++ webpack.config.js
@@ -0,0 +1,115 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const pkg = require('./package.json');
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'dist')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['', '.js'],
+    modulesDirectories: ['node_modules'],
+    root: PATHS.app
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index_template.html'
+    })
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        include: PATHS.app,
+        exclude: /node_modules|test/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js?/,
+        loader: 'babel',
+        include: PATHS.app,
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'cheap-module-source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: PATHS.app
+        },
+        {
+          test: /\.scss$/,
+          loaders: ['style', 'css', 'sass'],
+          include: PATHS.app
+        }
+      ]
+    }
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {
+    entry: {
+      vendor: Object.keys(pkg.dependencies)
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[chunkhash].js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: PATHS.app
+        },
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract('style', 'css!sass'),
+          include: PATHS.app
+        }
+      ]
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor'
+      }),
+      new ExtractTextPlugin('[name].[chunkhash].css'),
+      new CleanWebpackPlugin([PATHS.build]),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  });
+}
