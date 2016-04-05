74adfe00d9a4589bd5588c8648447d9b0040e6ed
Anton Grishun <agrishun@me.com>
2016-04-05 21:44:46 +0200

    reset styles

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import Root from './root';
+import './styles/index.css';
 
 injectTapEventPlugin();
 
