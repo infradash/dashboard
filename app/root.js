2f2a901c25b50171a7c32e65e3ecce04cd8a0b8f
Anton Grishun <agrishun@me.com>
2016-04-10 18:25:05 +0200

    routing changes

--- app/root.js
+++ app/root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRedirect, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 import store from './store';
@@ -17,14 +17,11 @@ const Root = () => {
   return (
     <Provider store={store}>
       <Router history={history}>
-        <Route
-          path="/"
-          onEnter={authRouteResolver(store.getState)}
-          component={App}
-        >
-          <IndexRoute component={Dashboard} />
-          <Route path="login" component={LoginForm} />
-          <Route path="accounts" component={Accounts} />
+        <Route path="/" onEnter={authRouteResolver(store.getState)} component={App}>
+          <IndexRedirect to="/accounts" />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/accounts" component={Accounts} />
         </Route>
       </Router>
     </Provider>
