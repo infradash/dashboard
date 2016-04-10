7bea579a962eeb974e603683348aec7f7ab95e60
Anton Grishun <agrishun@me.com>
2016-04-10 13:10:22 +0200

    github redirect url taken from window object

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 export const GITHUB_AUTH_API_URL = 'https://github.com/login/oauth/authorize';
 export const GITHUB_CLIENT_ID = 'ad470ea302c28c25941c';
 export const GITHUB_CLIENT_SECRET = 'c5a63ddaa0e13eed97d459c2245a2361a36889eb';
-export const REDIRECT_URI = 'http://localhost:8080/';
+export const REDIRECT_URI = window.location.origin;
 export const API_URL = 'https://accounts.qor.io/v1';
 export const NAVIGATION_WIDTH = 200;
