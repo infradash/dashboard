f098ae3c441ca7d2ce2beaa94c8d4ef29637ced4
Anton Grishun <agrishun@me.com>
2016-04-10 18:18:03 +0200

    fixed header

--- app/components/layout/Header.js
+++ app/components/layout/Header.js
@@ -1,11 +1,7 @@
 import React, { PropTypes } from 'react';
 import AppBar from 'material-ui/lib/app-bar';
 import FlatButton from 'material-ui/lib/flat-button';
-// import LinearProgress from 'material-ui/lib/linear-progress';
-// import IconButton from 'material-ui/lib/icon-button';
-// import NavigationArrowBack from 'material-ui/lib/svg-icons/navigation/arrow-back';
 import layoutStyles from 'styles/layout.css';
-// let loadingBar = this.props.isAuthenticating ? <LinearProgress mode="indeterminate" /> : null;
 
 export function Header({
     titleText,
@@ -16,6 +12,7 @@ export function Header({
 }) {
   return (
     <AppBar
+      className={layoutStyles.header}
       title={<span {...isAuthenticated ? { className: layoutStyles.title } : {}}>{titleText}</span>}
       showMenuIconButton={isAuthenticated}
       {...isAuthenticated ? {
