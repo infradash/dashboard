28e29dcb171f4e8705ad83fab66387bef19d493d
Anton Grishun <anton.grishun@kpn.com>
2016-04-29 12:37:34 +0200

    accounts removed from reducer

--- app/core/reducer.js
+++ app/core/reducer.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import { authReducer } from 'core/auth';
-import { accountsRecuder } from 'core/accounts';
 import { generalReducer } from 'core/general';
 
 export const reducer = combineReducers({
   routing: routerReducer,
   auth: authReducer,
-  accounts: accountsRecuder,
   general: generalReducer,
 });
