dd6e71f503841f0c2b37f3493fabfe38e01a335f
Anton Grishun <agrishun@me.com>
2016-05-12 14:14:03 +0200

    abstraction for network requests and response validation

--- app/utils/network.js
+++ app/utils/network.js
@@ -1,19 +1,71 @@
-export function checkHttpStatus(response) {
-  if (response.status < 200 || response.status >= 300) {
-    throw new Error(response.statusText);
-  }
-  return response;
+/* eslint no-confusing-arrow: ["error", {allowParens: true}]*/
+import fetch from 'isomorphic-fetch';
+import { API_PREFIX } from 'config';
+import store from 'store';
+import {
+  dataRequest,
+  dataRequestFailed,
+  dataRequestSuccessful,
+} from 'core/app';
+
+export function validateResponseCode(response) {
+  return new Promise((resolve, reject) => {
+    const { status, statusText } = response;
+    if (status < 200 || status >= 300) {
+      reject(statusText);
+    } else {
+      resolve(response);
+    }
+  });
 }
 
-export function checkResponse(response) {
-  if (response.error) {
-    throw new Error(response.error);
-  }
-  return response;
+export function validateResponseBody(response) {
+  return new Promise((resolve, reject) => {
+    if (response.error) {
+      reject(response.error);
+    } else {
+      resolve(response);
+    }
+  });
 }
 
-export function getAuthHeaders(token = '') {
-  return {
-    Authorization: `Bearer ${token}`,
+export function getHeaders() {
+  const { auth: { token } } = store.getState();
+  const headers = {
+    'Content-Type': 'application/json',
   };
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+  return headers;
+}
+
+export function createRequestPromise(endpoint, method, data = null) {
+  const isGetRequest = method.toUpperCase() === 'GET';
+  const requestObject = {
+    method,
+    headers: getHeaders(),
+    body: !isGetRequest ? JSON.stringify(data) : null,
+  };
+
+  return new Promise((resolve, reject) => {
+    store.dispatch(dataRequest());
+    const request = fetch(`${API_PREFIX}${endpoint}`, requestObject)
+      .then(validateResponseCode)
+      .then(response => (isGetRequest ? response.json() : response))
+      .then(validateResponseBody)
+      .then(response => {
+        setTimeout(() => {
+          store.dispatch(dataRequestSuccessful());
+          resolve(response);
+        }, 200);
+      })
+      .catch(error => {
+        setTimeout(() => {
+          store.dispatch(dataRequestFailed(error.toString()));
+          reject(error);
+        }, 200);
+      });
+    return request;
+  });
 }
