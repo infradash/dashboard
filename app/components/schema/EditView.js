1f9219a730f66fa7211914b28d7812cb6bd01d66
Anton Grishun <agrishun@me.com>
2016-05-12 20:42:44 +0200

    EditView refactoring, callback after action added

--- app/components/schema/EditView.js
+++ app/components/schema/EditView.js
@@ -1,47 +1,94 @@
 import React, { PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import equal from 'deep-equal';
 import Card from 'material-ui/lib/card/card';
 import CardActions from 'material-ui/lib/card/card-actions';
 import CardText from 'material-ui/lib/card/card-text';
 import RaisedButton from 'material-ui/lib/raised-button';
 import { SchemaForm, utils } from 'react-schema-form';
 
-export class EditView extends React.Component {
-  state = {
-    model: this.props.model,
+import { SCHEMA_INITIAL_ACTION_NAME } from 'config';
+import { showModalWindow } from 'core/app';
+
+class EditView extends React.Component {
+  static propTypes = {
+    methods: PropTypes.object,
+    schema: PropTypes.object,
+    showModalWindow: PropTypes.func,
   };
 
+  state = {
+    model: this.props.methods[SCHEMA_INITIAL_ACTION_NAME] || {},
+  };
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return !equal(this.state, nextState);
+  }
+
   onModelChange = (key, val) => {
-    this.setState({ model: utils.selectOrSet(key, this.props.model, val) });
+    this.setState({ model: utils.selectOrSet(key, this.state.model, val) });
     // refactor
   }
 
+  createCallback(callbackParams) {
+    if (!callbackParams) {
+      return () => ({});
+    }
+    return () => {
+      this.props.showModalWindow(callbackParams);
+    };
+  }
+
+  callSchemaMethod = (name, callback = {}) => {
+    const { success = null, fail = null } = callback;
+    const onSuccess = this.createCallback(success);
+    const onFail = this.createCallback(fail);
+    this.props.methods[name]()
+      .then(onSuccess)
+      .catch(onFail);
+  }
+
   render() {
+    const { fields, actions } = this.props.schema;
     return (
-      <Card>
-        <CardText>
-          <SchemaForm
-            schema={this.props.schema}
-            model={this.state.model}
-            onModelChange={this.onModelChange}
-          />
-        </CardText>
-        <CardActions>
-          {this.props.actions.map((action, index) => (
-            <RaisedButton
-              primary
-              key={index}
-              label={action.name}
-              onTouchTap={() => action.fn()}
+      <div>
+        <Card>
+          <CardText>
+            <SchemaForm
+              schema={fields}
+              model={this.state.model}
+              onModelChange={this.onModelChange}
             />
-          ))}
-        </CardActions>
-      </Card>
+          </CardText>
+          <CardActions>
+            {Object.keys(actions)
+              .filter(name => name !== SCHEMA_INITIAL_ACTION_NAME)
+              .map((name, index) => {
+                const { label, method, callback } = actions[name];
+                return (
+                  <RaisedButton
+                    primary
+                    key={index}
+                    label={label || method}
+                    onTouchTap={() => this.callSchemaMethod(name, callback)}
+                  />
+                );
+              })}
+          </CardActions>
+        </Card>
+      </div>
     );
   }
 }
 
-EditView.propTypes = {
-  model: PropTypes.object,
-  schema: PropTypes.object,
-  actions: PropTypes.array,
-};
+function mapDispatchToProps(dispatch) {
+  return {
+    showModalWindow: bindActionCreators(showModalWindow, dispatch),
+  };
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(EditView);
