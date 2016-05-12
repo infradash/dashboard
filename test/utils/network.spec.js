f3b505d3bf6439aa3768d948b9640a30bb008d50
Anton Grishun <agrishun@me.com>
2016-05-12 13:56:39 +0200

    network test spec, should be fixed according to the new implementation

--- test/utils/network.spec.js
+++ test/utils/network.spec.js
@@ -1,36 +1,46 @@
-import { expect, assert } from 'chai';
+import chai, { expect, assert } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+chai.should();
+
 import {
-  checkResponse,
-  checkHttpStatus,
+  validateResponse,
   getAuthHeaders,
 } from '../../app/utils/network';
 
 
 describe('network helpers', () => {
-  describe('checkHttpStatus', () => {
-    const statusSuccessful = { status:200 };
+  describe('validateResponse', () => {
+    const responseMock = {
+      response: {
+        json: new Promise((resolve, reject) => {
+          resolve('some text');
+        })
+      }
+    }
+    const statusSuccessful = Object.assign({}, responseMock, { status:200 });
     const statusNoAccess = { status:403, statusText:'User has no access' };
-
-    it('should check response object and return it if http status is correct', () => {
-      expect(checkHttpStatus(statusSuccessful)).eql(statusSuccessful);
-    });
-
-    it('should throw an error if http status is not successful', () => {
-      expect(() => checkHttpStatus(statusNoAccess)).to.throw("User has no access");
-    });
-  });
-
-  describe('checkResponse', () => {
     const responseOk = { data:[] };
     const responseError = { error: 'user has no access' };
 
-    it('should check response object and return it if no error property presents', () => {
-      expect(() => checkResponse(responseOk)).to.not.throw();
+    it('should resolve promise it if http status is correct', (done) => {
+      validateResponse(statusSuccessful).should.eventually.equal("foo").notify(done);
+//      return validateResponse(statusSuccessful).should.eventually.equal(4);
     });
 
-    it('should throw an error if response has error property', () => {
-      expect(() => checkResponse(responseError)).to.throw('user has no access');
+    it('should reject the promise if http status is not successful', () => {
+      console.log(validateResponse(statusNoAccess));
+      expect(validateResponse(statusNoAccess)).to.eventually.be.resolved;
     });
+
+    // it('should check response object and return it if no error property presents', () => {
+    //   expect(validateResponse(responseOk)).to.not.throw();
+    // });
+    //
+    // it('should throw an error if response has error property', () => {
+    //   expect(validateResponse(responseError)).to.throw('user has no access');
+    // });
+
   });
 
   describe('getAuthHeaders', () => {
