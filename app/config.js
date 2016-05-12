afb264647cdf1eb3504533ef10af71ef24afb22e
Anton Grishun <agrishun@me.com>
2016-05-12 14:01:27 +0200

    API_URL renamed to API_PREFIX

--- app/config.js
+++ app/config.js
@@ -2,5 +2,5 @@ export const GITHUB_AUTH_API_URL = 'https://github.com/login/oauth/authorize';
 export const GITHUB_CLIENT_ID = 'ad470ea302c28c25941c';
 export const GITHUB_CLIENT_SECRET = 'c5a63ddaa0e13eed97d459c2245a2361a36889eb';
 export const REDIRECT_URI = window.location.origin;
-export const API_URL = 'https://accounts.qor.io/v1';
+export const API_PREFIX = 'https://accounts.qor.io/v1';
 export const NAVIGATION_WIDTH = 200;
