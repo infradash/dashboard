7481832112bb0f3ae6835221cbabf06decb831b2
Anton Grishun <agrishun@me.com>
2016-05-12 20:39:02 +0200

    modalWindow actions

--- app/core/app/actions.js
+++ app/core/app/actions.js
@@ -1,11 +1,29 @@
-export const CLEAR_ERROR_MESSAGE = 'CLEAR_ERROR_MESSAGE';
+export const CLOSE_ERROR_MESSAGE = 'CLOSE_ERROR_MESSAGE';
 export const DATA_REQUEST = 'DATA_REQUEST';
 export const DATA_REQUEST_SUCCESSFUL = 'DATA_REQUEST_SUCCESSFUL';
 export const DATA_REQUEST_FAILED = 'DATA_REQUEST_FAILED';
+export const SHOW_MODAL_WINDOW = 'SHOW_MODAL_WINDOW';
+export const CLOSE_MODAL_WINDOW = 'CLOSE_MODAL_WINDOW';
 
-export function clearErrorMessage() {
+export function closeErrorMessage() {
   return {
-    type: CLEAR_ERROR_MESSAGE,
+    type: CLOSE_ERROR_MESSAGE,
+  };
+}
+
+export function closeModalWindow() {
+  return {
+    type: CLOSE_MODAL_WINDOW,
+  };
+}
+
+export function showModalWindow({ message, redirect = null }) {
+  return {
+    type: SHOW_MODAL_WINDOW,
+    payload: {
+      message,
+      redirect,
+    },
   };
 }
 
--- app/core/app/reducer.js
+++ app/core/app/reducer.js
@@ -1,20 +1,34 @@
 import {
-  CLEAR_ERROR_MESSAGE,
+  CLOSE_ERROR_MESSAGE,
+  CLOSE_MODAL_WINDOW,
+  SHOW_MODAL_WINDOW,
   DATA_REQUEST,
   DATA_REQUEST_SUCCESSFUL,
   DATA_REQUEST_FAILED,
 } from './actions';
 
 const initialState = {
+  modalWindowParams: {},
   isLoading: false,
   error: null,
 };
 
 export function appReducer(state = initialState, action) {
   switch (action.type) {
-    case CLEAR_ERROR_MESSAGE:
+    case CLOSE_ERROR_MESSAGE:
       return Object.assign({}, state, {
         error: null,
+      });
+    case CLOSE_MODAL_WINDOW:
+      return Object.assign({}, state, {
+        modalWindowParams: {},
+      });
+    case SHOW_MODAL_WINDOW:
+      return Object.assign({}, state, {
+        modalWindowParams: {
+          message: action.payload.message,
+          redirect: action.payload.redirect,
+        },
       });
     case DATA_REQUEST:
       return Object.assign({}, state, {
