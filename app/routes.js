1f4dbe84aafa5f77ae74f431c877345d2ccd439d
Anton Grishun <agrishun@me.com>
2016-05-13 15:44:36 +0200

    general route to resolve pages with schema

--- app/routes.js
+++ app/routes.js
@@ -1,35 +1,26 @@
 import store from './store';
-
+import Schema from 'components/schema';
 import { authRouteResolver } from 'core/auth';
-import { SchemaHistory } from 'components/schema';
-import { createRouteWithProps } from 'utils/react';
 
 import {
   App,
-  Dashboard,
   LoginForm,
   NotFound,
 } from 'views';
-
-import { data } from 'json/routes.json';
-const dynamicRoutes = data.map(route => createRouteWithProps(route, SchemaHistory));
 
 const routes = {
   path: '/',
   component: App,
   onEnter: authRouteResolver(store.getState),
-  indexRoute: { onEnter: (nextState, replace) => replace('/dashboard') },
   childRoutes: [
     {
       path: '/login',
       component: LoginForm,
     },
     {
-      path: '/dashboard',
-      name: 'Dashboard',
-      component: Dashboard,
+      path: '/generated/:name',
+      component: Schema,
     },
-    ...dynamicRoutes,
     {
       path: '*',
       component: NotFound,
