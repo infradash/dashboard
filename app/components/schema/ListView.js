2b641e0894200f120404adabb427877313aa97f2
Anton Grishun <agrishun@me.com>
2016-05-13 15:55:25 +0200

    ListView refactored, using native links with queries in url instead of javascript callback

--- app/components/schema/ListView.js
+++ app/components/schema/ListView.js
@@ -1,36 +1,56 @@
 import React, { PropTypes } from 'react';
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
+import { Link } from 'react-router';
 import { getProperty } from 'utils';
+import layoutStyles from 'styles/layout.css';
 import { SCHEMA_INITIAL_ACTION_NAME } from 'config';
 
-export default function ListView({ methods, schema, onTouchTap }) {
-  const { fields: { name, value, location } } = schema;
-  const model = methods[SCHEMA_INITIAL_ACTION_NAME] || [];
-  return (
-    <List>
-      {model.map((entity, index) => {
-        const data = JSON.stringify({
-          location,
-          params: {
-            [value]: getProperty(value, entity),
-          },
-        });
-        return (
-          <ListItem
-            key={index}
-            value={data}
-            primaryText={getProperty(name, entity)}
-            onTouchTap={onTouchTap}
-          />
-        );
-      })}
-    </List>
-  );
-}
+export default class ListView extends React.Component {
+  static propTypes = {
+    schema: PropTypes.object.isRequired,
+    methods: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+  }
 
-ListView.propTypes = {
-  schema: PropTypes.object.isRequired,
-  methods: PropTypes.object.isRequired,
-  onTouchTap: PropTypes.func.isRequired,
-};
+  state = {
+    model: [],
+  }
+
+  componentWillMount() {
+    this.props.methods[SCHEMA_INITIAL_ACTION_NAME]().then(response => {
+      this.setState({ model: response });
+    });
+  }
+
+  render() {
+    const { schema, location: { pathname } } = this.props;
+    const { fields: { name, value, location } } = schema;
+    return (
+      <List>
+        {this.state.model.map((entity, index) => {
+          const query = {
+            location,
+            [value]: getProperty(value, entity),
+          };
+          return (
+            <ListItem
+              key={index}
+              innerDivStyle={{ padding: 0 }}
+              children={
+                <Link
+                  key={index}
+                  style={{ padding: 16 }}
+                  className={layoutStyles.menuLink}
+                  to={{ pathname, query }}
+                >
+                  {getProperty(name, entity)}
+                </Link>
+              }
+            />
+          );
+        })}
+      </List>
+    );
+  }
+}
