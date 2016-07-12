ea6fff0f8fb99912830a74190da455f568efba32
Anton Grishun <agrishun@me.com>
2016-07-13 00:21:48 +0200

    Add a fix for preventing infinite rendering

--- app/views/SchemaBuiler.js
+++ app/views/SchemaBuiler.js
@@ -1,10 +1,17 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { SchemaResolver } from '../components/schema';
 
-export default function Schema(props) {
-  return (
-    <SchemaResolver location={props.location} />
-  );
+export default class Schema extends Component {
+
+  shouldComponentUpdate(nextProps) {
+    return this.props.location.query !== nextProps.location.query;
+  }
+
+  render() {
+    return (
+      <SchemaResolver location={this.props.location} />
+    );
+  }
 }
 
 Schema.propTypes = {
