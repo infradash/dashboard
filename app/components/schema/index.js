b41a214b491cb952220983cc7db4ed42c2b21f83
Anton Grishun <agrishun@me.com>
2016-09-23 16:28:11 +0200

    HttpParamsForm replaced by more simple implementation

--- app/components/schema/index.js
+++ app/components/schema/index.js
@@ -1,5 +1,4 @@
 export { default as SchemaResolver } from './SchemaResolver';
 export { default as SchemaView } from './SchemaView';
 export { default as ViewActions } from './ViewActions';
-export { default as HttpParamsForm } from './HttpParamsForm';
 export * from './views';
