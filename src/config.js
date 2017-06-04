674c51a5926f010fdc56122b7a625413de6446ec
Anton Grishun <agrishun@me.com>
2017-06-04 23:36:24 +0200

    path fix

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // @flow
 export const NAVIGATION_WIDTH = 200;
 export const SCHEMA_INITIAL_ACTION_NAME = 'initial';
-export const HOST_PATH = `${window.location.origin}${window.location.pathname}`;
+export const HOST_PATH = `${window.location.origin}${window.location.pathname}`.slice(0, -1);
 export const DEFAULT_HEADERS = {
   'Content-Type': 'application/json',
 };
